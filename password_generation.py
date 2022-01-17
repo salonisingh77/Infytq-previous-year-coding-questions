f2e534beb5f02ef109267257309653b3adc1e532
salonisingh77 <85999118+salonisingh77@users.noreply.github.com>
2022-01-17 22:57:39 +0530

    Update password_generation.py

--- password_generation.py
+++ password_generation.py
@@ -1,5 +1,9 @@
 
-# Online Python - IDE, Editor, Compiler, Interpreter
+#Problem: Take input from user in the given format (consist of name and code), find max digit from Code which is less or equal to the length of String and put that place Char in final String if there is no any digit found which not satisfy the condition that simply put ‘X’.
+#Sample Input:
+#Abhishek:34848,Mayur:4739,Friends:2949,Yeah:9889
+#Sample Output:
+#kueX
 
 input_str=input().split(",")
 out=""
